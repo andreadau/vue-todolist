1d7d979119db02618328ef917735ca3f3064d28f
Andrea <dauriaandrea99@gmail.com>
2020-12-16 18:47:44 +0100

    ! END !

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,15 @@ let vm = new Vue({
     el: "#app",
     data : {
         img : "img/logo.png",
+        newTodo: "",
+        arrTodo: ["Studiare Vue.js","Studiare React.js","Studiare Typescript"],
+    },
+    methods: {
+        addTodo(){
+            this.arrTodo.push(this.newTodo);
+        },
+        remove(index){
+            this.arrTodo.splice(index,1);
+        }
     }
 });
